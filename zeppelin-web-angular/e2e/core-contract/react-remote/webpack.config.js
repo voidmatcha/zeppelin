df83843a35ca4c28b06d53b36b1afb2fcb4ef983
YONGJAE LEE <dev.yongjaelee@gmail.com>
2026-08-31 21:03:48 +0900

    [ZEPPELIN-6674] Prove Shared Notebook Core port identity

---
+++ zeppelin-web-angular/e2e/core-contract/react-remote/webpack.config.js
@@ -0,0 +1,65 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const path = require('path');
+
+const webRoot = path.resolve(__dirname, '../../..');
+const reactRemoteRoot = path.resolve(webRoot, 'projects/zeppelin-react');
+const ModuleFederationPlugin = require(
+  path.join(reactRemoteRoot, 'node_modules/webpack/lib/container/ModuleFederationPlugin')
+);
+
+module.exports = {
+  entry: './empty.ts',
+  context: __dirname,
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    modules: [path.resolve(reactRemoteRoot, 'node_modules'), path.resolve(webRoot, 'node_modules'), 'node_modules'],
+    alias: {
+      '@zeppelin/notebook-core': path.resolve(webRoot, 'projects/zeppelin-notebook-core/src/public-api.ts')
+    }
+  },
+  resolveLoader: {
+    modules: [path.resolve(reactRemoteRoot, 'node_modules'), path.resolve(webRoot, 'node_modules'), 'node_modules']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            configFile: path.resolve(__dirname, 'tsconfig.json'),
+            transpileOnly: true
+          }
+        },
+        exclude: /node_modules/
+      }
+    ]
+  },
+  output: {
+    clean: true,
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/assets/react/',
+    scriptType: 'text/javascript',
+    uniqueName: 'notebookCorePortProof'
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      exposes: {
+        './NotebookCorePortProbe': './NotebookCorePortProbe'
+      },
+      filename: 'remoteEntry.js',
+      name: 'reactApp'
+    })
+  ]
+};
