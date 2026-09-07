6452a47d7cf8f61bc652183f676b5306646834f0
YONGJAE LEE <dev.yongjaelee@gmail.com>
2026-09-08 05:11:36 +0900

    [ZEPPELIN-6687] Reset Notebook Core before reconnect reload

--- zeppelin-web-angular/src/app/pages/workspace/notebook/notebook.component.ts
+++ zeppelin-web-angular/src/app/pages/workspace/notebook/notebook.component.ts
@@ -840,6 +840,7 @@ export class NotebookComponent extends MessageListenersManager implements OnInit
         if (!connected) {
           return;
         }
+        this.notebookCoreRouteAdapter.reloadCurrentRoute();
         this.requestCurrentNote();
         this.cdr.markForCheck();
         const { noteId } = params;
