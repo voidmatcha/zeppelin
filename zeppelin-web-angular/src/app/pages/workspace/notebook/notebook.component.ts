782dcb6855755ed91c73571293a8fab3fa22d800
YONGJAE LEE <dev.yongjaelee@gmail.com>
2026-09-08 05:12:50 +0900

    [ZEPPELIN-6687] Sync personalized mode from notebook events

--- zeppelin-web-angular/src/app/pages/workspace/notebook/notebook.component.ts
+++ zeppelin-web-angular/src/app/pages/workspace/notebook/notebook.component.ts
@@ -340,6 +340,7 @@ export class NotebookComponent extends MessageListenersManager implements OnInit
     this.note.info = data.info;
     this.notebookCoreRouteAdapter.acceptNoteUpdated(data.name);
     this.initializeLookAndFeel(this.note);
+    this.notebookCoreRouteAdapter.acceptPersonalizedMode(this.note.config.personalizedMode === 'true');
     this.cdr.markForCheck();
   }
 
